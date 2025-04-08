aa51afc68329b9462ad7e9464b54e87e29fbb899
Amine Mastouri <amin.mastouri01@gmail.com>
2025-04-08 02:29:01 +0100

    creating openai singleton

--- src/openai/openai.module.ts
+++ src/openai/openai.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { OpenaiController } from './openai.controller';
 import { OpenaiService } from './openai.service';
+import OpenAI from 'openai';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [ConfigModule],
   controllers: [OpenaiController],
-  providers: [OpenaiService]
+  providers: [OpenaiService,{
+    provide:OpenAI,
+    useFactory: (configService: ConfigService) => {
+       new OpenAI({
+        apiKey: configService.getOrThrow('OPENAI_API_KEY'),
+      });
+    },
+    inject: [ConfigService],
+  } ],
 })
 export class OpenaiModule {}
