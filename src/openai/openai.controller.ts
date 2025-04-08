a6c450badba5c1e5ad88c844bb4abd9de85def2a
Amine Mastouri <amin.mastouri01@gmail.com>
2025-04-08 03:04:37 +0100

    creating chatcompletion API

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,10 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes } from '@nestjs/common';
 import { CreateChatCompletionRequest } from './dto/ChatCompletion.request';
+import { OpenaiService } from './openai.service';
 
 @Controller('openai')
 export class OpenaiController {
+    constructor(private readonly openaiService: OpenaiService) {}
+
     @Post('ChatCompletion')
     async chatCompletion(@Body()body : CreateChatCompletionRequest) {
-       
+
+       this.openaiService.createChatCompletion(body.messages);
     }
 }
--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import OpenAI from 'openai';
+import { ChatCompletionMessageParam } from 'openai/resources/chat';
+import { ChatCompletionMessage } from './dto/ChatCompletion.request';
 
 @Injectable()
-export class OpenaiService {}
+export class OpenaiService {
+    constructor(private readonly openai :OpenAI) {}
+
+    async createChatCompletion(messages:ChatCompletionMessage[]){
+        return await this.openai.chat.completions.create({
+            messages: messages as  ChatCompletionMessageParam[] ,
+            model: 'gpt-4'
+    });
+}
+}
